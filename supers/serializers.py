1fc159d76d8542db6f5d3a5c3600ee1901b5a0e7
achilleswargod <65618715+achilleswargod@users.noreply.github.com>
2022-03-30 10:45:32 -0500

    Last commit was meant before deadline

--- supers/serializers.py
+++ supers/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
-from .models import Supers
+from .models import Super
 
 class SuperSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Supers
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type']
+        model = Super
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
+        depth = 1
+
+    super_type_id = serializers.IntegerField(write_only = True)
+
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,69 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from super_types.models import SuperType
+from .serializers import SuperSerializer
+from .models import Super
+
+
+class SuperList(APIView):
+    """
+    List all Supers, or create a new super.
+    """
+    def get(self, request):
+        supers = Super.objects.all()
+        super_types = SuperType.objects.all()
+        custom_response_dictionary = []
+
+        super_param = request.query_params.get('type')
+        if super_param:
+            supers = supers.filter(super_type__type=super_param)
+            serializer = SuperSerializer(supers, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            for super_type in super_types:
+                supers = Super.objects.filter(super_type__id = super_type.id)
+                serializer = SuperSerializer(supers,many = True)
+                custom_response_dictionary[super_type.type] = {
+                    "supers": serializer.data,
+                }
+            return Response(custom_response_dictionary)
+
+       
+
+    def post(self, request):
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+
+class SuperDetail(APIView):
+    """
+    Retrieve, update or delete a super instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Super.objects.get(pk=pk)
+        except Super.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        super = self.get_object(pk)
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+      
+
+    def delete(self, request, pk, format=None):
+        super = self.get_object(pk)
+        super.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
